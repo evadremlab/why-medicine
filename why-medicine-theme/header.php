fc05a53d7c31041b36033d66769fc3b8294ce4e7
David Balmer <david.balmer@transsight.com>
2018-01-31 16:03:42 -0800

    Mobile mods done

--- why-medicine-theme/header.php
+++ why-medicine-theme/header.php
@@ -43,7 +43,7 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'why-medicine' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">&#9776;</button>
 			<?php
 				wp_nav_menu( array(
 					'theme_location' => 'menu-1',
@@ -51,6 +51,16 @@
 				) );
 			?>
 		</nav><!-- #site-navigation -->
+
+		<aside id="slide-nav">
+			<?php
+				wp_nav_menu( array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'slide-menu',
+				) );
+			?>
+		</aside>
+
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
